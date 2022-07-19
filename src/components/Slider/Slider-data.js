d24b85f80fbf0345e3556a521f85ce60e2062261
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-07-19 10:10:27 -0700

    Initial commit

--- src/components/Slider/Slider-data.js
+++ src/components/Slider/Slider-data.js
@@ -7,13 +7,13 @@ const SliderData = [
     id: 1,
     image: image1,
     heading: 'Header 1',
-    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining'
   },
   {
     id: 2,
     image: image2,
     heading: 'Header 2',
-    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining.'
   },
   {
     id: 3,
--- src/screens/header/header.js
+++ src/screens/header/header.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect} from 'react';
 
+import Button from 'react-bootstrap/Button';
+
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 import './Header.scss'
@@ -10,17 +12,35 @@ const Header = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideLength = SliderData.length;
 
+  const autoScroll = true;
+  let slideInterval;
+  const intervalTime = 5000;
+
   const nextSlide = () => {
     setCurrentSlide(currentSlide === slideLength -1 ? 0 : currentSlide + 1);
   };
 
   const prevSlide = () => {
     setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide -1);
-  }
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function auto(){
+    slideInterval = setInterval(nextSlide, intervalTime);
+  };
 
   useEffect(() => {
     setCurrentSlide(0)
   }, []);
+
+  useEffect(() => {
+    if(autoScroll){
+      auto();
+    };
+
+    return () => clearInterval(slideInterval);
+  }, [auto, autoScroll, currentSlide, slideInterval]);
+
 
   return (
    <div className='slider'>
@@ -32,15 +52,15 @@ const Header = () => {
             <div className={index === currentSlide ? "slide current" : "slide"} key={index}>
               {
                 index === currentSlide && (
-                  <>
+                  <div className='slider-box'>
                   <img src={slide.image} alt="slide" />
                   <div className='content'>
                     <h2>{slide.heading}</h2>
                     <p>{slide.desc}</p>
                     <hr />
-                    <button className='--btn--btn-primary'>Get Started</button>
+                    <Button variant="primary">Learn more</Button>
                   </div>
-                  </>
+                  </div>
                 )
               }
             </div>
