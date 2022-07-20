49b0065984a4eb89d9a2b7481399e55b4c26a461
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-07-20 00:26:42 -0700

    Initial commit

--- src/screens/navbar/navbar.js
+++ src/screens/navbar/navbar.js
@@ -4,8 +4,22 @@ import './navbar.scss';
 
 const navbar = () => {
   return (
-    <div className='navbar'>
-      navbar...
+    <div className='navigation'>
+      <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+
+      <label for="navi-toggle" className="navigation__button">MENU</label>
+
+      <div className="navigation__background">&nbsp;</div>
+
+      <nav className="navigation__nav">
+        <ul className="navigation__list">
+          <li className="navigation__item"><a href="#" className="navigation__link">About me</a></li>
+          <li className="navigation__item"><a href="#" className="navigation__link">About me</a></li>
+          <li className="navigation__item"><a href="#" className="navigation__link">About me</a></li>
+          <li className="navigation__item"><a href="#" className="navigation__link">About me</a></li>
+          <li className="navigation__item"><a href="#" className="navigation__link">About me</a></li>
+        </ul>
+      </nav>
     </div>
   )
 };
